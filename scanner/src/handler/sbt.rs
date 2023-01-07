37b5903681a6b46504249325a16d0dd01b6e541b
traitmeta <traitmeta@gmail.com>
2023-01-07 15:53:15 +0800

    add trait for hanlder

--- scanner/src/handler/sbt.rs
+++ scanner/src/handler/sbt.rs
@@ -7,11 +7,37 @@ function:
 
 minting --> minted --> burning --> burned
 ^    ｜                     |
-|    ｜                     |         
+|    ｜                     |
 -- mint_fail           burn_fail
 */
 
+pub trait EventHandler {
+    fn handle(&self) -> Result<(), error>;
+}
+
+struct SBTInfo {
+    chain_id: u64,
+    contract: string,
+    sbt_id: string,
+    token_id: string,
+    status: u8,
+    lifetime: SBTLifetime,
+}
+
+struct SBTLifetime {
+    sbt_info_id: u64,
+    opr_type: u8,
+    opr_timestamp: i32,
+}
 
 pub struct SBT {
+    insert_queue: Vec<SBTInfo>,
+    update_queue: Vec<SBTInfo>,
+    delete_queue: Vec<SBTInfo>,
+}
 
+impl EventHandler for SBT {
+    fn handle(&self) -> Result<(), error> {
+        OK(())
+    }
 }
