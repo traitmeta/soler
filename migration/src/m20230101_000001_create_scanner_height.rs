ae7122bad626695c5c1444eeb87e8d561d73e193
traitmeta <s.xue.xian.c@gmail.com>
2023-02-01 16:05:52 +0800

    change migration

--- migration/src/m20230101_000001_create_scanner_height.rs
+++ migration/src/m20230101_000001_create_scanner_height.rs
@@ -6,44 +6,43 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(ScannerHeight::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(Post::Id)
+                        ColumnDef::new(ScannerHeight::Id)
                             .integer()
                             .not_null()
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .col(ColumnDef::new(ScannerHeight::TaskName).string().not_null())
+                    .col(ColumnDef::new(ScannerHeight::ChainName).string().not_null())
+                    .col(ColumnDef::new(ScannerHeight::Height).integer().not_null())
+                    .col(ColumnDef::new(ScannerHeight::CreatedAt).date().not_null())
+                    .col(ColumnDef::new(ScannerHeight::UpdatedAt).date().not_null())
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(ScannerHeight::Table).to_owned())
             .await
     }
 }
 
 /// Learn more at https://docs.rs/sea-query#iden
 #[derive(Iden)]
-enum Post {
+enum ScannerHeight {
     Table,
     Id,
-    Title,
-    #[iden = "text"]
-    Text,
+    TaskName,
+    ChainName,
+    Height,
+    CreatedAt,
+    UpdatedAt,
 }
