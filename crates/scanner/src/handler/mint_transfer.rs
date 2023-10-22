17848ead9dd2b2f8e865e70f2376fb2357d4eafd
traitmeta <traitmeta@gmail.com>
2023-10-22 16:33:25 +0800

    add test

--- crates/scanner/src/handler/mint_transfer.rs
+++ crates/scanner/src/handler/mint_transfer.rs
@@ -22,7 +22,8 @@ fn parse(logs: &[Log]) -> Vec<MintTransfer> {
     let mut mint_transfers: Vec<MintTransfer> = Vec::new();
 
     for log in logs.iter() {
-        if log.topics[0].as_bytes() == consts::BRIDGE_HASH.as_bytes() {
+        let first_topic = format!("0x{}", hex::encode(log.topics[0].as_bytes()));
+        if first_topic.as_str() == consts::BRIDGE_HASH {
             if let Some(params) = parse_params(log) {
                 mint_transfers.push(params);
             }
@@ -46,4 +47,45 @@ fn parse_params(log: &Log) -> Option<MintTransfer> {
     }
 
     None
+}
+
+#[cfg(test)]
+mod tests {
+    use ethers::types::{Log, H160};
+
+    use super::parse;
+
+    #[test]
+    fn test_parse() {
+        let src = r#"
+        [
+            {
+                "address": "0x867305d19606aadba405ce534e303d0e225f9556",
+                "topics": [
+                    "0x3c798bbcf33115b42c728b8504cff11dd58736e9fa789f1cda2738db7d696b2a",
+                    "0x0000000000000000000000009a4a90e2732f3fa4087b0bb4bf85c76d14833df1",
+                    "0x0000000000000000000000007301cfa0e1756b71869e93d4e4dca5c7d0eb0aa6"
+                ],
+                "data": "0x0000000000000000000000000000000000000000000000001bc16d674ec80000",
+                "blockHash": "0x1000e552ce843557bbf120df4d075fae2b56b0f57f3621b88dffc9a210ef223e",
+                "blockNumber": "0x972413",
+                "transactionHash": "0x1d5066d30ff3404a9306733136103ac2b0b989951c38df637f464f3667f8d4ee",
+                "transactionIndex": "0xc",
+                "logIndex": "0x34",
+                "removed": false
+            }
+        ]
+        "#;
+        let log: Vec<Log> = serde_json::from_str(src).unwrap();
+        let parsed_data = parse(&log);
+        assert!(parsed_data.len() == 1);
+        let to: H160 = "0x9a4a90e2732f3fa4087b0bb4bf85c76d14833df1"
+            .parse()
+            .unwrap();
+        let from: H160 = "0x7301cfa0e1756b71869e93d4e4dca5c7d0eb0aa6"
+            .parse()
+            .unwrap();
+        assert!(parsed_data[0].from == from.as_bytes().to_vec());
+        assert!(parsed_data[0].to == to.as_bytes().to_vec());
+    }
 }
