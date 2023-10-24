477680d06a1d60f5fcd69eda003e63c865f815ab
traitmeta <traitmeta@gmail.com>
2023-10-24 11:39:42 +0800

    remove twitter code

--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -1,5 +1,4 @@
 pub mod auth;
 pub mod handlers;
 pub mod router;
-pub mod twitter;
 pub mod validater;
--- crates/api/src/twitter/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod api_v2;
-
-use twitter_v2::authorization::{BearerToken, Oauth2Token};
-use twitter_v2::query::{TweetField, UserField};
-use twitter_v2::Authorization;
-use twitter_v2::TwitterApi;
