efba3efcce27ed73a2bdb5f280540361e4baff77
traitmeta <traitmeta@gmail.com>
2023-10-27 12:07:58 +0800

    add learn

---
+++ crates/learn/src/mastering/iter/iter.rs
@@ -0,0 +1,72 @@
+use std::usize;
+struct Primes {
+    limit: usize,
+}
+
+impl Primes {
+    fn iter(&self) -> PrimesIter {
+        PrimesIter {
+            index: 2,
+            computed: compute_primes(self.limit),
+        }
+    }
+    fn new(limit: usize) -> Primes {
+        Primes { limit }
+    }
+}
+
+// 计算在limit之内的素数并放在一个Vec中
+fn compute_primes(limit: usize) -> Vec<bool> {
+    let mut sieve = vec![true; limit];
+    let mut m = 2;
+    while m * m < limit {
+        if sieve[m] {
+            for i in (m * 2..limit).step_by(m) {
+                sieve[i] = false;
+            }
+        }
+        m += 1;
+    }
+    sieve
+}
+
+struct PrimesIter {
+    index: usize,
+    computed: Vec<bool>,
+}
+impl Iterator for PrimesIter {
+    type Item = usize;
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            self.index += 1;
+            if self.index > self.computed.len() - 1 {
+                return None;
+            } else if self.computed[self.index] {
+                return Some(self.index);
+            } else {
+                continue;
+            }
+        }
+    }
+}
+
+fn useage() -> Vec<usize> {
+    let primes = Primes::new(100);
+    let mut res: Vec<usize> = vec![];
+    for i in primes.iter() {
+        res.push(i);
+    }
+    res
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::useage;
+
+    #[test]
+    fn test_custom_iter() {
+        let res: Vec<usize> = useage();
+        let expect:Vec<usize>  = vec![3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97];
+        assert_eq!(res,expect)
+    }
+}
---
+++ crates/learn/src/mastering/sync/sync.rs
@@ -0,0 +1,113 @@
+use std::sync::mpsc::{self, channel};
+use std::sync::{Arc, Mutex, RwLock};
+use std::thread;
+
+fn run_mpsc_sync_channel() {
+    let (tx, rx) = mpsc::sync_channel(1);
+    let tx_clone = tx.clone();
+
+    let _ = tx.send(0);
+
+    thread::spawn(move || {
+        let _ = tx.send(1);
+    });
+
+    thread::spawn(move || {
+        let _ = tx_clone.send(2);
+    });
+
+    println!("Received {} via the channel", rx.recv().unwrap());
+    println!("Received {} via the channel", rx.recv().unwrap());
+    println!("Received {} via the channel", rx.recv().unwrap());
+    println!("Received {:?} via the channel", rx.recv());
+}
+
+// 出现了死锁，注释其中一个joinHandler，可以解锁
+
+fn run_mpsc_multi_tx() {
+    let (tx, rx) = channel();
+    let mut childs = vec![];
+
+    let join_handle = thread::spawn(move || {
+        // loop{
+        //     match rx.recv() {
+        //         Ok(n) => println!("Received {}", n),
+        //         Err(e) => {
+        //             println!("Received {}", e);
+        //             panic!("{}", e)
+        //         }
+        //     }
+        // }
+        while let Ok(n) = rx.recv() {
+            println!("Received {}", n);
+        }
+    });
+    for i in 0..10 {
+        let v = tx.clone();
+        let t = thread::spawn(move || {
+            v.send(i).unwrap();
+        });
+        childs.push(t);
+    }
+
+    for c in childs {
+        c.join().unwrap();
+    }
+    // join_handle.join().unwrap();
+}
+
+fn run_mutex() {
+    let vec = Arc::new(Mutex::new(vec![]));
+    let mut childs = vec![];
+    for i in 0..5 {
+        let mut v = vec.clone();
+        let t = thread::spawn(move || {
+            let mut v = v.lock().unwrap();
+            v.push(i);
+        });
+        childs.push(t);
+    }
+    for c in childs {
+        c.join().unwrap();
+    }
+    println!("{:?}", vec);
+}
+
+// RwLock on some systems such as Linux, suffers from the writer starvation problem.
+fn run_rwlock() {
+    let m = RwLock::new(5);
+    let c = thread::spawn(move || {
+        {
+            *m.write().unwrap() += 1;
+        }
+        let updated = *m.read().unwrap();
+        updated
+    });
+    let updated = c.join().unwrap();
+    println!("{:?}", updated);
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn test_run_mutex() {
+        run_mutex();
+    }
+
+    #[test]
+    fn test_run_rwlock() {
+        run_rwlock();
+    }
+
+    #[test]
+    fn test_run_mpsc_multi_tx() {
+        run_mpsc_multi_tx();
+    }
+
+    #[test]
+    fn test_run_mpsc_sync_channel() {
+        run_mpsc_sync_channel();
+    }
+}
