8efb07025b58187667c6baa06f090fa2d95fd216
traitmeta <traitmeta@gmail.com>
2023-10-27 14:14:44 +0800

    feat: add mspc for client

---
+++ learn/my-redis/src/bin/client.rs
@@ -0,0 +1,53 @@
+use bytes::Bytes;
+use mini_redis::client;
+use tokio::sync::mpsc;
+
+#[derive(Debug)]
+enum Command {
+    Get { key: String },
+    Set { key: String, val: Bytes },
+}
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = mpsc::channel(32);
+    let tx2 = tx.clone();
+
+    let t1 = tokio::spawn(async move {
+        let cmd = Command::Get {
+            key: "hello".to_string(),
+        };
+        tx.send(cmd).await.unwrap();
+    });
+
+    let t2 = tokio::spawn(async move {
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            val: "bar".into(),
+        };
+        tx2.send(cmd).await.unwrap();
+    });
+
+    // while let Some(message) = rx.recv().await {
+    //     println!("GOT = {}", message);
+    // }
+
+    let manager = tokio::spawn(async move {
+        let mut client = client::connect("127.0.0.1:16379").await.unwrap();
+
+        while let Some(cmd) = rx.recv().await {
+            use Command::*;
+            match cmd {
+                Get { key } => {
+                    client.get(&key).await;
+                }
+                Set { key, val } => {
+                    client.set(&key, val).await;
+                }
+            }
+        }
+    });
+
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
+}
