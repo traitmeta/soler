eb40cf54bf9eb9c03716bdcf07ee01e0e31a28dd
traitmeta <traitmeta@gmail.com>
2023-10-27 13:29:10 +0800

    feat: add multi thread for server

--- learn/my-redis/src/main.rs
+++ learn/my-redis/src/main.rs
@@ -1,4 +1,9 @@
-use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+
+use mini_redis::{
+    Command::{self, Get, Set},
+    Connection, Frame,
+};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
@@ -13,11 +18,25 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
-    let mut connection = Connection::new(socket);
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+    let mut db = HashMap::new();
 
-        let response = Frame::Error("unimplemented".to_string());
+    let mut connection = Connection::new(socket);
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+
         connection.write_frame(&response).await.unwrap();
     }
 }
