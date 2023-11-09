e7f3022318f76e69db7a8d8eb0ea2a7cf4eeb50b
traitmeta <traitmeta@gmail.com>
2023-11-09 15:38:17 +0800

    DEV => Token balance => on demand

--- crates/scanner/src/indexer/token_balances.rs
+++ crates/scanner/src/indexer/token_balances.rs
@@ -1,8 +1,12 @@
+use anyhow::Result;
 use bigdecimal::BigDecimal;
 use chrono::Utc;
 use ethers::types::U256;
-use std::{collections::HashMap, str::FromStr};
+use std::{collections::HashMap, str::FromStr, time::Duration};
+use tokio::time;
+use tracing::error;
 
+use entities::address_current_token_balances::Model as CurrentTokenBalanceModel;
 use entities::address_token_balances::Model as TokenBalanceModel;
 
 use crate::{
@@ -15,7 +19,7 @@ use crate::{
 pub async fn fetch_token_balances_from_blockchain(
     balance_reader: BalanceReader,
     token_balances: Vec<TokenBalanceModel>,
-) -> Result<HashMap<String, Vec<TokenBalanceModel>>, String> {
+) -> Result<HashMap<String, Vec<TokenBalanceModel>>> {
     println!("fetching token balances, count: {}", token_balances.len());
 
     let mut fetched_token_balances = vec![];
@@ -78,4 +82,60 @@ fn get_token_balance_key(token_balance: &TokenBalanceModel) -> String {
             .unwrap_or(&BigDecimal::from(0)),
         hex::encode(token_balance.address_hash.clone()),
     )
+}
+
+pub async fn fetch_current_token_balance(
+    balance_reader: BalanceReader,
+    token_balance: &mut CurrentTokenBalanceModel,
+) -> Result<()> {
+    let backoff = 1;
+
+    loop {
+        let req = TokenBalanceRequest {
+            token_contract_address_hash: format!(
+                "0x{}",
+                hex::encode(token_balance.token_contract_address_hash.clone())
+            ),
+            address_hash: format!("0x{}", hex::encode(token_balance.address_hash.clone())),
+            block_number: token_balance.block_number,
+            token_id: token_balance
+                .token_id
+                .as_ref()
+                .map(|token_id| U256::from(token_id.to_string().as_bytes())),
+            token_type: token_balance
+                .token_type
+                .as_ref()
+                .map_or(consts::TokenKind::None, |t| match t.as_str() {
+                    consts::ERC20 => consts::TokenKind::ERC20,
+                    consts::ERC721 => consts::TokenKind::ERC721,
+                    consts::ERC1155 => consts::TokenKind::ERC1155,
+                    _ => consts::TokenKind::None,
+                }),
+        };
+
+        let result = balance_reader.token_balance_call_contract(req).await;
+        token_balance.value_fetched_at = Some(Utc::now().naive_utc());
+        token_balance.inserted_at = Utc::now().naive_utc();
+        token_balance.updated_at = Utc::now().naive_utc();
+        token_balance.old_value = token_balance.value.clone();
+        match result {
+            Ok(balance) => {
+                token_balance.value =
+                    Some(BigDecimal::from_str(balance.to_string().as_str()).unwrap());
+                return Ok(());
+            }
+            Err(e) => {
+                if backoff > 64 {
+                    error!("failed to accept socket after retry: {}", e);
+                    return Err(e);
+                } else {
+                    error!("failed to accept socket: {}", e);
+                }
+            }
+        }
+
+        time::sleep(Duration::from_secs(backoff)).await;
+
+        let _ = backoff << 2;
+    }
 }
